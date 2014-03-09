0dee46c88bd70beb32dc23cafae0823ead5d9a22
alec <alec.hulce@gmail.com>
2014-03-09 15:13:41 -0700

    u

--- _deploy/test.sh
+++ _deploy/test.sh
@@ -20,7 +20,6 @@ else
   echo "sire repo deployed"
 fi
 
-# `ssh ubuntu@ec2-54-84-87-232.compute-1.amazonaws.com "sudo cat /root/sire/secrets | grep githubHookAuthToken | head -n1 | grep -oP '\".+\"'"`
 t=`ssh ubuntu@$serverName "sudo cat /root/sire/secrets | grep githubHookAuthToken | head -n1 | grep -oP '\".+\"'"`
 if [ "$t" != "\"$githubHookAuthToken\"" ]; then
   echo "config missing githubHookAuthToken"
