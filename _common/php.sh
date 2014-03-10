f758cab3146999efcdd0b51adb051139032c2dd7
alec <alec.hulce@gmail.com>
2014-03-10 16:49:59 -0700

    u

--- _common/php.sh
+++ _common/php.sh
@@ -4,5 +4,6 @@ apt-get -y install php5-fpm php5-cli php5-mysql
 
 if [ "`grep 'listen = /var/run/php5-fpm.sock' /etc/php5/fpm/pool.d/www.conf`" != "" ]; then
   sed -i -e 's/\/var\/run\/php5-fpm.sock/127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
-  service php5-fpm restart
 fi
+
+service php5-fpm restart
