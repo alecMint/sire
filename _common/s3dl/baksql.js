0419006f9d2a455e90b70c73bcb5d57cc3879fdf
alec <alec.hulce@gmail.com>
2014-03-09 17:01:35 -0700

    u

--- _common/s3dl/baksql.js
+++ _common/s3dl/baksql.js
@@ -13,15 +13,23 @@ var bucket = argv.b || argv.bucket
 if (!bucket || !dbName) throw "db and bucket required";
 
 fs.mkdir(tmpDir,function(err){
-  if (err) {
-    return console.log(err,err.code);
-  }
+  if (err && err.code != 'EEXIST')
+    return console.log(err);
   var fn = dbName+'.'+Date.now()+'.sql.gz'
   ,path = tmpDir+fn;
   cp.exec('mysqldump --opt -hlocalhost -uroot '+dbName+' | gzip > '+path,function(err){
-    if (err) {
+    if (err)
       return console.log(err);
-    }
+    s3cmd(['put','s3://'+path.join([bucket,fn])],function(err){
+      if (err)
+        console.log('failed to push '+path+' to s3',err);
+      else
+        console.log('successfully pushed '+path+' to s3');
+      fs.unlink(path,function(err){
+        if (err)
+          return console.log('failed to clean up');
+      });
+    });
   });
 });
 
