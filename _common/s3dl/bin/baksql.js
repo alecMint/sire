c60107e515a4cc79cf19d0fe8dc86c2cb7ab8333
alec <alec.hulce@gmail.com>
2014-04-10 21:30:25 -0700

    u

--- _common/s3dl/bin/baksql.js
+++ _common/s3dl/bin/baksql.js
@@ -6,6 +6,7 @@ var sql = require('../sql')
 
 var bucket = argv.b || argv.bucket
 ,dbName = argv.d || argv.database
+,numBaks = argv.n || argv.numbaks || 30
 ;
 if (!bucket || !dbName) throw "db and bucket required";
 
@@ -13,7 +14,7 @@ sql.backup(dbName,bucket,function(err){
   if (err)
     return console.log(err);
   console.log(dbName+' backed to '+bucket);
-  sql.clean(bucket,dbName,3,function(err,filesDeleted){
+  sql.clean(bucket,dbName,numBaks,function(err,filesDeleted){
     if (err)
       return console.log('error cleaning s3',err);
     console.log('cleaned remote',filesDeleted);
