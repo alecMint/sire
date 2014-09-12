b2759ade84579b0caf798b4027f3e61f0ad850a4
alec <alec.hulce@gmail.com>
2014-09-12 15:52:48 -0700

    u

--- fabfitfun-migrate-wordpress/config.sh
+++ fabfitfun-migrate-wordpress/config.sh
@@ -1,3 +1,4 @@
 
 export installDir='/var/www/migrate-wordpress'
-export gitRepo="https://github.com/fabfitfun/migrate-wordpress.git"
+#export gitRepo="https://github.com/fabfitfun/migrate-wordpress.git"
+export gitRepo="git@github.com:fabfitfun/migrate-wordpress.git"
