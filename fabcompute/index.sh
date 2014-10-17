fa950592be33bd191fd33246880513be100c08fb
alec <alec.hulce@gmail.com>
2014-10-16 20:06:37 -0700

    u

---
+++ fabcompute/index.sh
@@ -0,0 +1,30 @@
+
+startpwd=`pwd`
+
+. ../secrets
+
+# modules
+#../_common/nginx.sh
+#../_common/php.sh
+#../_common/mysql.sh
+../_common/nodejs.sh
+../_common/forever.sh
+
+
+# repo
+if [ ! -d "$installDir" ]; then
+  mkdir -p "$installDir"
+  git clone $gitRepo "$installDir"
+fi
+cd "$installDir"
+git checkout master
+git pull origin master
+
+
+# deploy hook service
+IP=`public_ip`
+echo '[{"repo":"'$installDir'","branch":"master"}]' > $installDir'/hooky.json'
+cd $startpwd/hooky
+npmi
+forever_run "./index.js -t $githubHookAuthToken -a $IP -c $installDir/hooky.json"
+cd $startpwd
