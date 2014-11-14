e93d7eec88d1a7586291b3324a8cdbc6d16b7097
alec <alec.hulce@gmail.com>
2014-11-13 21:51:07 -0800

    u

--- bin/angel.sh
+++ bin/angel.sh
@@ -4,7 +4,9 @@ cd $dir
 . ../_common/util.sh
 
 
+echo "about2: running = forever_is_running \"$1\""
 running=`forever_is_running "$1"`
+echo "running = $running"
 if [ "$running" == "" ]; then
   echo $1" is not running!"
   forever_run "$1"
