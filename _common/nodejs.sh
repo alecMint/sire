4064756e13d77c228d4ac453bb5a1cbffa72bb36
alec <alec.hulce@gmail.com>
2014-11-13 16:12:00 -0800

    u

--- _common/nodejs.sh
+++ _common/nodejs.sh
@@ -8,14 +8,14 @@ PREFIX="/usr/local"
 CVERSION=`node -v 2> /dev/null`
 
 if [ "$CVERSION" != "$VERSION" ]; then
+  # might need this to avoid npm install error
+  mkdir -p /root/.node-gyp/$VERSION
+
 	# mkdir -p "/usr/local" && curl http://nodejs.org/dist/v0.10.32/node-v0.10.32-linux-x64.tar.gz | tar xzvf - --strip-components=1 -C "/usr/local"
   mkdir -p "$PREFIX" && \
   curl http://nodejs.org/dist/$VERSION/node-$VERSION-$PLATFORM-$ARCH.tar.gz \
     | tar xzvf - --strip-components=1 -C "$PREFIX"
   # what is the wiggles for biggles?
-
-  # might need this to avoid npm install error
-  #mkdir -p /root/.node-gyp/$VERSION
 
   # forever was failing...
   ln -s $PREFIX/bin/node /usr/bin/node
