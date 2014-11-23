1ba1bd709550f8cf533493e550e1547ab35890ed
alec <alec.hulce@gmail.com>
2014-11-23 09:25:51 -0800

    u

--- ace/index.sh
+++ ace/index.sh
@@ -60,7 +60,7 @@ gen_add_line_to_file "$installDir/web/config.local.php" awsRegion "\$awsRegion='
 
 gen_add_line_to_file "$installDir/config.local.json" sesKey "{"
 gen_add_line_to_file "$installDir/config.local.json" sesKey "\"sesKey\"=\"$sesKey\""
-gen_add_line_to_file "$installDir/config.local.json" sesSecret "\"sesSecret\"=\"$sesSecret\""
-gen_add_line_to_file "$installDir/config.local.json" awsRegion "\"awsRegion\"=\"$awsRegion\""
+gen_add_line_to_file "$installDir/config.local.json" sesSecret ",\"sesSecret\"=\"$sesSecret\""
+gen_add_line_to_file "$installDir/config.local.json" awsRegion ",\"awsRegion\"=\"$awsRegion\""
 gen_add_line_to_file "$installDir/config.local.json" sesKey "}"
 
