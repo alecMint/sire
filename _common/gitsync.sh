c82e693ed112138f303369929e4e1c1c50bf51bd
alec <alec.hulce@gmail.com>
2014-11-24 00:55:26 -0800

    u

--- _common/gitsync.sh
+++ _common/gitsync.sh
@@ -9,7 +9,7 @@ git checkout -f $branch
 git pull origin $branch
 git submodule update
 npm install
-echo "SUP! $dir/post-gitsync.sh"
+
 if [ -f "$dir/post-gitsync.sh" ]; then
 	$dir/post-gitsync.sh "$dir" "$branch"
 fi
