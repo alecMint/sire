cb0a4b25aa7356145abc8abf8e4dda473340a8db
alec <alec.hulce@gmail.com>
2014-11-24 01:21:46 -0800

    u

--- alechulce/index.sh
+++ alechulce/index.sh
@@ -25,14 +25,18 @@ git checkout master
 git pull origin master
 
 
+# deploy hook service
+#IP=`public_ip`
+#echo '[{"repo":"'$installDir'","branch":"master"}]' > $installDir'/hooky.json'
+#cd $startpwd/hooky
+#npmi
+#forever_run "./index.js -t $githubHookAuthToken -a $IP -c $installDir/hooky.json"
+#cd $startpwd
+
+# until i fix multiple github hooks issue...
+gitsync_cron "$installDir" "master"
+
+
 # test cname
 localhost_add_cname 'local.alechulce.com'
 
-
-# deploy hook service
-IP=`public_ip`
-echo '[{"repo":"/var/www/alechulce","branch":"master"}]' > '/var/www/alechulce/hooky.json'
-cd $startpwd/hooky
-npmi
-forever_run "./index.js -t $githubHookAuthToken -a $IP -c /var/www/alechulce/hooky.json"
-cd $startpwd
