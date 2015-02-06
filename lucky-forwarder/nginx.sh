f5422315311dabf4e649ad969d2e0f6ddfc623b5
alec <alec.hulce@gmail.com>
2015-02-06 13:08:03 -0800

    ༼ ͡■ل͜ ͡■༽

--- lucky-forwarder/nginx.sh
+++ lucky-forwarder/nginx.sh
@@ -16,6 +16,54 @@ server {
 	autoindex off;
 
 	access_log /var/log/nginx/lucky-forwarder_access_log.log;
+	error_log /var/log/nginx/lucky-forwarder_error_log.log;
+
+	gzip on; # use gzip compression
+	gzip_min_length 1100;
+	gzip_buffers 4 8k; 
+	gzip_proxied any; # enable proxy for the fcgi requests
+	gzip_types text/plain text/css application/x-javascript text/javascript application/json; 
+
+	# pass php to fastcgi
+	location ~ \.php\$ {
+		fastcgi_index index.php;
+		include /etc/nginx/fastcgi_params;
+		fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+		if (-e \$request_filename) { # check if requested path exists
+			fastcgi_pass $nginxBackend;
+		}
+	}
+
+	location / {
+		# set expire headers for assets
+		if (\$request_uri ~* "\.(ico|css|js|gif|jpe?g|png)\$") {
+			expires max;
+		}
+		include /etc/nginx/fastcgi_params;
+		fastcgi_param SCRIPT_FILENAME \$document_root/index.php;
+		fastcgi_param SCRIPT_NAME /index.php;
+		# pass nonexistants to index.php
+		if (!-f \$request_filename) {
+			fastcgi_pass $nginxBackend;
+			break;
+		}
+	}
+}
+
+server {
+	listen 443 default;
+
+	server_name www.luckymag.com luckymag.com m.luckymag.com promo.luckymag.com legacy.luckymag.com;
+	root $installDir/web;
+	autoindex off;
+
+	access_log /var/log/nginx/lucky-forwarder_access_log.log;
+	error_log /var/log/nginx/lucky-forwarder_error_log.log;
+
+	server_tokens off;
+	ssl on;
+	ssl_certificate $installDir/ssl/luckymag.pem;
+	ssl_certificate_key $installDir/ssl/luckymag-key.pem;
 
 	gzip on; # use gzip compression
 	gzip_min_length 1100;
