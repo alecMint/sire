661753803aff9399a4e2445496e86cf0c3ab28d5
alec <alec.hulce@gmail.com>
2015-02-06 13:50:41 -0800

    ʕ；•`ᴥ•´ʔ

--- lucky-forwarder/test.sh
+++ lucky-forwarder/test.sh
@@ -1,15 +1,23 @@
 
 if [ "`/etc/init.d/nginx configtest 2>&1 | grep failed`" != "" ]; then
-  echo "nginx conf failed"
-  exit 1
+	echo "nginx conf failed"
+	exit 1
 else
-  echo "nginx conf passed"
+	echo "nginx conf passed"
 fi
 
 
 if [ "`cat ${installDir}/.git/config | grep -oP "${gitRepo}$"`" != $gitRepo ]; then
-  echo "repo missing"
-  exit 1
+	echo "repo missing"
+	exit 1
 else
-  echo "repo deployed"
+	echo "repo deployed"
+fi
+
+
+if [ "`s3cmd 2>&1 | grep 'not found'`" != "" ]; then
+	echo "s3cmd not installed"
+	exit 1
+else
+	echo "s3cmd installed"
 fi
