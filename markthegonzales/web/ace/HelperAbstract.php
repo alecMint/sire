1b14b32ce1ce4e19deae7558a9d71ab7f7c7418b
alec <alec.hulce@gmail.com>
2015-02-14 00:23:05 -0800

    ಠ╭╮ಠ

--- markthegonzales/web/ace/HelperAbstract.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ace;
-
-abstract class HelperAbstract {
-
-}
--- markthegonzales/web/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include dirname(__FILE__).'/bootshell.php';
-use \ace\Ace;
-
-
-// load wordpress
-define('WP_USE_THEMES', true);
-require(WEBROOT.'/wp-blog-header.php');
