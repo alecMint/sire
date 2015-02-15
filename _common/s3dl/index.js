d003ab4eb444f3b888000fb3d486fc734938c55c
alec <alec.hulce@gmail.com>
2015-02-14 20:21:43 -0800

    ಠ_ಠ

--- _common/s3dl/index.js
+++ _common/s3dl/index.js
@@ -88,7 +88,7 @@ function createServer(port){
 		var re = new RegExp('proxy_pass http://localhost:'+portConfig.target+';','g');
 		var newCnf = cnf.replace(re, 'proxy_pass http://localhost:'+portConfig.attempting+';');
 		console.log(newCnf);
-		fs.writeFileSync(portConfig.nginxCnf, newCnf);
+		fs.writeFileSync(portConfig.nginxCnf, [newCnf]);
 		cp.spawn('/etc/init.d/nginx','reload');
 	});
 }
