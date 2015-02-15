d0639caf72baa3c80efca9f506efe77695623c4a
alec <alec.hulce@gmail.com>
2015-02-14 18:06:52 -0800

    ( ﾟヮﾟ)

--- _common/util.sh
+++ _common/util.sh
@@ -99,7 +99,7 @@ forever_run(){
 }
 
 forever_stop(){
-	index=`forever_uid "$1"` # was using forever_index before, but had issues when stopping index 0
+	index=`forever_uid '$1'` # was using forever_index before, but had issues when stopping index 0
 	if [ "$index" == "" ]; then
 		echo "forever stop> $1 not running"
 	else
