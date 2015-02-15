433601d45e4ee3966218ce5da2a69d5c4e6aaacc
alec <alec.hulce@gmail.com>
2015-02-14 18:01:02 -0800

    ⊙▃⊙

--- _common/util.sh
+++ _common/util.sh
@@ -85,8 +85,7 @@ forever_run(){
 	fi
 
 	echo "forever_run sireDir: $sireDir"
-	#crontab_add "$torun" "* * * * * $sireDir/bin/angel.sh \"$torun\" >> /var/log/angel.log 2>&1"
-	crontab_add "$torun" "* * * * * /root/sire/bin/angel.sh \"$torun\" >> /var/log/angel.log 2>&1"
+	crontab_add "$torun" "* * * * * $sireDir/bin/angel.sh \"$torun\" >> /var/log/angel.log 2>&1"
 	echo "forever_stop '$torun'"
 	forever_stop "$torun" # needs to be super unique
 
