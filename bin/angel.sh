b3964d051ccec805146ca8e4ad98d305e58a26ba
alec <alec.hulce@gmail.com>
2015-02-14 18:32:02 -0800

    ¯(°_o)/¯

--- bin/angel.sh
+++ bin/angel.sh
@@ -3,6 +3,9 @@ dir=`dirname $0`
 cd $dir
 . ../_common/util.sh
 
+# hack: set $sireDir for forever_run
+sireDir=`realpath $dir/..`
+
 #echo "angel.sh \"$1\" "`date`
 running=`forever_is_running "$1"`
 if [ "$running" == "" ]; then
