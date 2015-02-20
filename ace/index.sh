df3b457d61077db89bf912d6ff79af0abf410418
alec <alec.hulce@gmail.com>
2015-02-20 06:22:41 -0800

    ≧☉_☉≦

--- ace/index.sh
+++ ace/index.sh
@@ -59,3 +59,16 @@ gen_add_line_to_file "$installDir/config.local.json" awsAccessSecret ",\"awsAcce
 gen_add_line_to_file "$installDir/config.local.json" awsRegion ",\"awsRegion\": \"$awsRegion\""
 gen_add_line_to_file "$installDir/config.local.json" '}'
 
+
+# rotate logs
+logrotate_log=$installDir/out/logrotate.log
+nginx_access_log=`grep access_log /etc/nginx/sites-enabled/ace | head -n1 | awk '{print $2}' | tr -d ';'`
+nginx_error_log=`grep error_log /etc/nginx/sites-enabled/ace | head -n1 | awk '{print $2}' | tr -d ';'`
+if [ "$nginx_access_log" != "" ] || [ "$nginx_error_log" != "" ]; then
+	cron="0 2 * * * /bin/bash $sireDir/bin/logrotate.sh 10 '$nginx_access_log' '$nginx_error_log' '$logrotate_log' 2>&1 >> '$logrotate_log' #ace_rotateLogs"
+	echo "installing crontab: $cron"
+	crontab_add '#ace_rotateLogs' "$cron"
+fi
+
+
+
