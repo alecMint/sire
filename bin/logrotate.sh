995c983b0e601411a31b26aa220ddc2e5c217e5a
alec <alec.hulce@gmail.com>
2015-02-20 06:10:39 -0800

    (❍ᴥ❍ʋ)

---
+++ bin/logrotate.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# ./bin/logrotate.sh 10 ./out/mylog.log ./out/anotherlog.log 2>&1 >> /var/log/logrotate.log
+
+date
+
+maxFiles=$1
+
+positiveInt='^[1-9][0-9]*$'
+if ! [[ "$maxFiles" =~ $positiveInt ]]; then
+	echo "invalid maxFiles arg"
+	exit 1
+fi
+
+if [ "`echo "$2" | grep '.sh'`" != "" ]; then
+	preDeletionHook=$2
+fi
+echo "preDeletionHook: $preDeletionHook"
+
+if [ ! -f /usr/bin/realpath ]; then
+	echo "realpath not found, making one"
+	realpath() {
+		echo `cd "${1}";pwd`
+	}
+fi
+
+
+rotate(){
+	logFile=$1
+	maxFiles=$2
+	for ((i=$maxFiles;i>=0;i--)); do
+		[ $i == 0 ] && suf='' || suf=".$i"
+		[ ! -f "$logFile$suf" ] && continue
+		if [ $i == $maxFiles ]; then
+			# if a preDeletionHook points to a script for shipping off to s3 or w/e...
+			echo "checking preDeletionHook: $preDeletionHook"
+			if [ -f "$preDeletionHook" ]; then
+				echo "running preDeletionHook..."
+				"$preDeletionHook" "$logFile$suf"
+			fi
+			echo "rm $logFile$suf"
+			rm "$logFile$suf"
+			continue
+		fi
+		echo "$logFile$suf > $logFile."$[$i+1]
+		mv "$logFile$suf" "$logFile."$[$i+1]
+	done
+
+	# if looks like nginx log, make nginx drop its file handler
+	if [ "`echo "$logFile" | grep nginx`" != "" ]; then
+		echo "identified nginx log file: $logFile"
+		if [ -f /var/run/nginx.pid ]; then
+			kill -USR1 `cat /var/run/nginx.pid` && sleep 1 && echo "dropped nginx file handler" || echo "failed to drop nginx file handler :("
+		else
+			echo "failed to drop nginx file handler: /var/run/nginx.pid is not a file"
+		fi
+	fi
+
+}
+
+n=0
+for arg in "$@"; do
+	n=$[n+1]
+	[ $n == 1 ] && continue
+	filePath=`/usr/bin/realpath "$arg"`
+	rotate "$filePath" $maxFiles
+done
+
+date
