b69c226ce84440e532eb6db31a7195d5a277a291
alec <alec.hulce@gmail.com>
2015-02-24 00:02:58 -0800

    ʕ；•`ᴥ•´ʔ

--- _common/nginx.sh
+++ _common/nginx.sh
@@ -3,3 +3,16 @@
 apt-get -y install nginx
 /etc/init.d/nginx start
 update-rc.d nginx defaults
+
+# disable default log rotation
+if [ -f /etc/logrotate.d/nginx ]; then
+	contents=`cat /etc/logrotate.d/nginx | tr -d '\n'`
+	if [ "$contents" != "" ]; then
+		echo "disabling default nginx log rotation"
+		mkdir -p /root/baks
+		bakFile=/root/baks/logrotate.d_nginx.$(date +"%Y%m%d_%H%M%S")
+		echo "placing current config in $bakFile"
+		cp /etc/logrotate.d/nginx $bakFile
+		echo '' > /etc/logrotate.d/nginx
+	fi
+fi
