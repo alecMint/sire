8ce005158542b545cde4a65b99ec27c49ad5fd2c
alec <alec.hulce@gmail.com>
2015-02-24 04:40:06 -0800

    ʕ；•`ᴥ•´ʔ

--- _common/s3dl.sh
+++ _common/s3dl.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-s3dlinstall_pwd=`pwd`
 mkdir -p $sireDir/_common/s3dl/node_modules
-cd $sireDir/_common/s3dl
-npm install
-cd "$s3dlinstall_pwd"
+npm install --prefix $sireDir/_common/s3dl
