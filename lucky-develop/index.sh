966c006be77f7362cf1d46de6c8869840cb6401a
alec <alec.hulce@gmail.com>
2015-05-08 15:23:53 -0700

    ಠ⌣ಠ

--- lucky-develop/index.sh
+++ lucky-develop/index.sh
@@ -2,8 +2,9 @@
 . ../secrets
 
 if [ ! -f /usr/local/bin/node ]; then
-	#ln -s /usr/bin/node /usr/local/bin/node
+	ln -s /usr/bin/node /usr/local/bin/node
 fi
 
 installDir=/var/www/platform-v2-lucky/current
-configure_hooky $installDir develop $githubHookAuthToken 9998 $installDir/restart.sh
+echo "githubHookAuthToken: $githubHookAuthToken"
+#configure_hooky $installDir develop $githubHookAuthToken 9998 $installDir/restart.sh
