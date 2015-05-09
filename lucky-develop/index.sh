5d8d4cacea013e295f02581de2568001115c97dc
alec <alec.hulce@gmail.com>
2015-05-08 17:05:02 -0700

    ಠ_ಠ

--- lucky-develop/index.sh
+++ lucky-develop/index.sh
@@ -6,8 +6,9 @@
 if [ ! -f /usr/local/bin/node ] && [ -f /usr/bin/node ]; then
 	ln -s /usr/bin/node /usr/local/bin/node
 fi
-if [ ! -f /usr/local/bin/forever ] && [ -f /usr/bin/forever ]; then
-	ln -s /usr/bin/forever /usr/local/bin/forever
+#if [ ! -f /usr/local/bin/forever ] && [ -f /usr/bin/forever ]; then
+if [ ! -f /usr/local/bin/forever ] && [ -f /usr/lib/node_modules/forever/bin/forever ]; then # /usr/bin/forever is a link
+	ln -s /usr/bin/forever /usr/lib/node_modules/forever/bin/forever
 fi
 
 installDir=/var/www/platform-v2/current
