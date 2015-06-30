99e937d146fdf8cd90cf69be6e8dce44dad4d18c
alec <alec.hulce@gmail.com>
2015-06-30 14:19:57 -0700

    (ง •̀_•́)ง

--- _common/gitsync.sh
+++ _common/gitsync.sh
@@ -1,5 +1,5 @@
 
-sig=`echo "$@" | sed -n 's/[^a-zA-Z0-9_-]/_/gp'`
+sig=`echo "$1" | sed -n 's/[^a-zA-Z0-9_-]/_/gp'`
 lockFile="/tmp/gitsync.$sig.lock"
 if [ -f "$lockFile" ]; then
 	echo "lock file found, exiting"
@@ -14,16 +14,17 @@ branch=$2
 cd $dir || (echo "dir $dir does not exist, exiting" && exit 1)
 git fetch
 git reset --hard HEAD
-git checkout -f origin/$branch
+#git checkout -f origin/$branch # is there a reason i had origin/...?
+git checkout -f $branch
 git pull origin $branch
 git submodule update
 
-if [ -f "$dir/package.json" ]; then
+if [ -f ./package.json ]; then
 	npm install
 fi
 
-if [ -f "$dir/post-gitsync.sh" ]; then
-	$dir/post-gitsync.sh "$dir" "$branch"
+if [ -f ./post-gitsync.sh ]; then
+	./post-gitsync.sh "$dir" "$branch"
 fi
 
 
