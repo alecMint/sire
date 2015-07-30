49d5969ce1145a213e3acd7ae8f0426d91e7362b
alec <alec.hulce@gmail.com>
2015-07-29 18:04:20 -0700

    ʕ•ᴥ•ʔ

--- wag-api/index.sh
+++ wag-api/index.sh
@@ -19,6 +19,7 @@ fi
 
 
 # @todo: move this to wagapi/install.sh
+cd "$installDir"
 /usr/bin/curl -sS https://getcomposer.org/installer | /usr/bin/php
 /usr/bin/php "$installDir/composer.phar" install
 # also maybe: php artisan migrate
