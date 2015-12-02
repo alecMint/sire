c1824f3267be4903fc8fc73e328b13684cbbf38f
alec <alec.hulce@gmail.com>
2015-12-02 13:54:54 -0800

    [raptor] add chef-config to raptor deploy - fix reference\; was incorrectly pointed at wagapi

--- raptor/chef-config.sh
+++ raptor/chef-config.sh
@@ -7,11 +7,11 @@ git clone git@github.com:waglabs/chef-deploy.git /tmp/chef-deploy
 
 
 # @todo: pick based on $CHEF_ENV instead of hardcoding DEV
-if [ -f /tmp/chef-deploy/wagapi/files/DEV.config.local.sh ]; then
-	cp -f /tmp/chef-deploy/wagapi/files/DEV.config.local.sh "$installDir/config.local.sh"
+if [ -f /tmp/chef-deploy/raptor/files/DEV.config.local.sh ]; then
+	cp -f /tmp/chef-deploy/raptor/files/DEV.config.local.sh "$installDir/config.local.sh"
 fi
-if [ -f /tmp/chef-deploy/wagapi/files/DEV.config.local.json ]; then
-	cp -f /tmp/chef-deploy/wagapi/files/DEV.config.local.json "$installDir/config.local.json"
+if [ -f /tmp/chef-deploy/raptor/files/DEV.config.local.json ]; then
+	cp -f /tmp/chef-deploy/raptor/files/DEV.config.local.json "$installDir/config.local.json"
 fi
 
 
