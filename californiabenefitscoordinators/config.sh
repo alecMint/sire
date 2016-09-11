8e3a8f84782e06afbba36998449263096c4eabde
alec <alec.hulce@gmail.com>
2016-09-10 19:36:44 -0700

    californiabenefitscoordinators

---
+++ californiabenefitscoordinators/config.sh
@@ -0,0 +1,5 @@
+
+export installDir='/var/www/californiabenefitscoordinators'
+export gitRepo="https://github.com/fluffybunnies/californiabenefitscoordinators.git"
+export mysqlDb='cbc_wp'
+#export googleAnalyticsId=''
---
+++ californiabenefitscoordinators/nginx.sh
@@ -0,0 +1,65 @@
+# nginx conf
+
+phpBackend="127.0.0.1:9000"
+
+cat > /etc/nginx/sites-available/californiabenefitscoordinators <<FILE
+server {
+	listen 80;
+	server_name californiabenefitscoordinators.com;
+	return 301 http://www.californiabenefitscoordinators.com\$request_uri;
+}
+server {
+	listen 80;
+
+	server_name www.californiabenefitscoordinators.com;
+	root /var/www/californiabenefitscoordinators/web;
+	autoindex off;
+
+	access_log /var/log/nginx/californiabenefitscoordinators_access_log.log;
+	error_log /var/log/nginx/californiabenefitscoordinators_error_log.log;
+
+	gzip on; # use gzip compression
+	gzip_min_length 1100;
+	gzip_buffers 4 8k; 
+	gzip_proxied any; # enable proxy for the fcgi requests
+	gzip_types text/plain text/css application/x-javascript text/javascript application/json; 
+
+	# s3 webwrite
+	location ~ ^/wp-content/uploads {
+		if (!-f \$request_filename) {
+			# 9991 = s3dl
+			proxy_pass http://localhost:9991; #s3dl
+			break;
+		}
+	}
+
+	# pass php to fastcgi
+	location ~ \.php\$ {
+		fastcgi_index index.php;
+		include /etc/nginx/fastcgi_params;
+		fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+		if (-e \$request_filename) { # check if requested path exists
+			fastcgi_pass $phpBackend;
+		}
+	}
+
+	location / {
+		# set expire headers for assets
+		if (\$request_uri ~* "\.(ico|css|js|gif|jpe?g|png)\$") {
+			expires max;
+		}
+		include /etc/nginx/fastcgi_params;
+		fastcgi_param SCRIPT_FILENAME \$document_root/index.php;
+		fastcgi_param SCRIPT_NAME /index.php;
+		# pass nonexistants to index.php
+		if (!-f \$request_filename) {
+			fastcgi_pass $phpBackend;
+			break;
+		}
+	}
+}
+FILE
+rm /etc/nginx/sites-enabled/default 2> /dev/null
+ln -f /etc/nginx/sites-available/californiabenefitscoordinators /etc/nginx/sites-enabled/californiabenefitscoordinators
+
+/etc/init.d/nginx reload
